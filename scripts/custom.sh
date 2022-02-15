169306a4e53bf11ab8fbc43e4eac45e80b7a6c99
Dickson Lee <30920372+gb0k@users.noreply.github.com>
2022-02-15 11:09:40 +0800

    custom.sh: add immortalwrt luci package

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -50,7 +50,8 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Comment Lean's Package
 #sed -i -e '1 s/^/#/' -i -e '2 s/^/#/' feeds.conf.default
+sed -i -e '2 s/^/#/' feeds.conf.default
 
 # Add immortalwrt's Package and Luci
 #echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
-#echo 'src-git luci https://github.com/immortalwrt/luci.git' >>feeds.conf.default
+echo 'src-git luci https://github.com/immortalwrt/luci.git' >>feeds.conf.default
